8c8163a4b7607e552446e72f1771286fd5bb1404
isaacBats <disa.indus@gmail.com>
2015-10-02 00:32:24 -0500

    Primera presentacion del menu

--- js/opera.js
+++ js/opera.js
@@ -1,5 +1,5 @@
 //intervalo de tiempo img slider, 5 segundos
-setInterval('avanzaSlide()', 5000);
+setInterval('avanzaSlide()', 3000);
 
 //Se declara el array con las clases de las  imagenes
 var arrayImag = new Array(".img1", ".img2", ".img3", ".img4", ".img5");
@@ -7,14 +7,14 @@ var contador = 0;
 
 function mostrar (img) {
 	$(img).ready(function(){
-		$(arrayImag[contador]).fadeIn(1500);
+		$(arrayImag[contador]).fadeIn(1000);
 	});
 }
 
 //para ocultar una imagen
 function ocultar (img) {
 	$(img).ready(function(){
-		$(arrayImag[contador]).fadeOut(1500);
+		$(arrayImag[contador]).fadeOut(1000);
 	});
 }
 
@@ -27,3 +27,26 @@ function avanzaSlide () {
 }
 
 // alert("Hola estoy en js");
+
+//Para el menu desplegable
+var contador = 1;
+ 
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+ 
+		if(contador == 1){
+			$('.menu').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('.menu').animate({
+				left: '-100%'
+			});
+		}
+ 
+	});
+ 
+};
